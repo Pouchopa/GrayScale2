ffa39f9aa43a9f3835ac0c2d8e2d7cb3371d5e90
Pouchopa <pouchopa67@gmail.comthomas.hannauer@altran.com>
2017-06-01 10:21:36 +0200

    Add GrayScale file

---
+++ GrayScale2.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import java.io.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class GrayScaleMultiple {
+   public static int compteur = 0;
+   public static void GrayScaleMultiple(String fileName) {
+	BufferedImage  image;
+	int width;
+	int height;
+      try {
+         File input = new File(fileName);
+         image = ImageIO.read(input);
+         width = image.getWidth();
+         height = image.getHeight();
+         
+         for(int i=0; i<height; i++){
+         
+            for(int j=0; j<width; j++){
+            
+               Color c = new Color(image.getRGB(j, i));
+               int red = (int)(c.getRed() * 0.299);
+               int green = (int)(c.getGreen() * 0.587);
+               int blue = (int)(c.getBlue() *0.114);
+               Color newColor = new Color(red+green+blue,
+               
+               red+green+blue,red+green+blue);
+               
+               image.setRGB(j,i,newColor.getRGB());
+            }
+         }
+         
+         File ouptut = new File(compteur + "-output.jpg");
+		 compteur++;
+         ImageIO.write(image, "jpg", ouptut);
+		 
+         
+      } catch (Exception e) {}
+   }
+   
+    public static void BlackAndWhite (String fileName) throws IOException {
+
+		File file = new File(fileName);
+		BufferedImage orginalImage = ImageIO.read(file);
+
+		BufferedImage blackAndWhiteImg = new BufferedImage(
+			orginalImage.getWidth(), orginalImage.getHeight(),
+			BufferedImage.TYPE_BYTE_BINARY);
+
+		Graphics2D graphics = blackAndWhiteImg.createGraphics();
+		graphics.drawImage(orginalImage, 0, 0, null);
+
+		ImageIO.write(blackAndWhiteImg, "jpg", new File(compteur + "-blackAndWhite.jpg")); 
+	}
+   
+   public static void main(String args[]) throws IOException 
+   {
+      GrayScaleMultiple(args[0]);
+	  BlackAndWhite(args[0]);
+	  GrayScaleMultiple(args[1]);
+	  BlackAndWhite(args[1]);
+   }
+}
